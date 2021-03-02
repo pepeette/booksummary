e3d54a1a89021acb96229322e19b87e6ca19a28c
pepeette <59664130+pepeette@users.noreply.github.com>
2021-03-02 14:04:36 +0800

    clean code app

--- EbookSummer.py
+++ EbookSummer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 #pip install EbookLib
-
 
 import ebooklib
 from ebooklib import epub
@@ -10,15 +8,12 @@ import bs4
 import streamlit as st
 import os
 
-
 # ## coding the app
-
 
 # Add a title
 st.title('Ebook summary')
 # Add some text
 st.text('upload your book in .epub format')
-
 
 # allow user input
 uploaded_file = st.file_uploader("Upload Files",type=['epub'])
@@ -26,22 +21,7 @@ if uploaded_file is not None:
     file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type,"FileSize":uploaded_file.size}
     st.write(file_details)
 
-
 # # SUMMARIZING THE WHOLE BOOK
-
-# ## 1- TRansforming the epub into text
-
-
-# Seeing the chapters
-# agenda = []
-# livre = epub.read_epub(uploaded_file)
-# for item in livre.get_items():
-#     if item.get_type() == ebooklib.ITEM_NAVIGATION:
-#             #print('----------------------------------')
-#         agenda.append(item.get_content())
-#             #print('==================================')
-        
-
 
 livre = epub.read_epub(uploaded_file)
 def epub2thtml(epub_path):
@@ -60,12 +40,8 @@ def epub2thtml(epub_path):
 #then we need to look for each :  h2 class="chn1_chapter_number_" for index // h3 class="ha1_head_var_1o" for text
 #maybe with beautifulsoup
 
-
-
 from bs4 import BeautifulSoup as soup
-blacklist = ['[document]','noscript','header','html','meta','head', 'input','script',
-# there may be more elements you don't want, such as "style", etc.
-]
+blacklist = ['[document]','noscript','header','html','meta','head', 'input','script']
 
 def chap2text(chap):
     output = ''
@@ -76,8 +52,6 @@ def chap2text(chap):
             output += '{} '.format(t)
     return output
 
-
-
 def thtml2ttext(thtml):
     Output = []
     for html in thtml:
@@ -85,24 +59,15 @@ def thtml2ttext(thtml):
         Output.append(text)
     return Output
 
-
 def epub2text(epub_path):
     chapters = epub2thtml(epub_path)
     ttext = thtml2ttext(chapters)
     return ttext
 
-
-
-
-
 text = epub2text(livre)
-
-
 print(type(text))
 
-
 # ## Now, we can apply the text PREPROCESSING
-
 
 import re
 
@@ -110,22 +75,17 @@ import re
 t = [re.sub(r'\[[0-9]*\]', ' ', i) for i in text]
 t = [re.sub(r'\s+', ' ', i) for i in t]
 
-
 # Removing special characters and digits
 t1 = [re.sub(r'[^a-zA-Z]', ' ', i) for i in t]
 t1 = [re.sub(r'\s+', ' ', i) for i in t1]
-
 
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 
-
-
 # tokenizing 
 sentence_list = [sent_tokenize(i) for i in t]
 sentence_list[3]
-
 
 # find frequecy of occurrence
 
@@ -140,15 +100,11 @@ for chapter in t1:
             else:
                 word_frequencies[word] += 1
 
-
 # second we calculate the weighted frequency
 
 maximum_frequency = max(word_frequencies.values())
-
 for word in word_frequencies.keys():
     word_frequencies[word] = (word_frequencies[word]/maximum_frequency)
-
-
 
 # third we expand this weighted F to the sentences to score them
 
@@ -166,37 +122,15 @@ for chapter in sentence_list[1:]:
 
 # ## HERE IS THE SUMMARY
 
-
-# # first choose the length of your summary for the app (streamlit)
-# x = st.slider('Select a length')
-# st.write(x, 'squared is', x * x)
-
-
-
 # retrieving the TOP 5 sentences 
 
 import heapq
 summary_sentences = heapq.nlargest(5, sentence_scores, key=sentence_scores.get)
-
 summary = ' '.join(summary_sentences)
 print(summary)
 
-
-# In[ ]:
-
-
 # display text on app
 st.write(summary)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
 
 
 
